f4b1d1d2506d2b6863f04e596dde3bcca7fce6bf
Enzzo D.N <enzzodiasnogueira2@gmail.com>
2023-11-27 14:42:52 -0300

    Update remove.py

--- model/remove.py
+++ model/remove.py
@@ -1,1 +1,43 @@
-from selenium import webdriver
+from selenium import webdriver 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+import pyautogui as p
+
+def remove():
+    driver = webdriver.Chrome()
+    driver.get('https://www.remove.bg/pt-br')
+
+    # ----------------- upload ---------------
+    WebDriverWait(
+        EC.presence_of_element_located(By.XPATH, '/html/body/div[1]/main/div[2]/div[1]/div/div/div/div[2]/div[2]/button')
+    )
+    botao = driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/div[1]/div/div/div/div[2]/div[2]/button')
+    botao.click()
+    time.sleep(1)
+    
+    nome = 'C:\\Users\\enzzo.nogueira\\Desktop\\bot_selenium\\nome.png'
+    p.locateCenterOnScreen(nome)
+    while nome != True:
+        p.locateCenterOnScreen(nome)
+        time.sleep(1)
+    p.click()
+    p.typewrite('C:\Users\enzzo.nogueira\Desktop\python_logo.png' + '\n')
+
+    # --------- download --------------
+    WebDriverWait(
+        EC.presence_of_element_located(By.XPATH, '/html/body/div[1]/main/div/div/div/div/div[2]/div[2]/div[2]/button')
+    )
+    download = driver.find_element(By.XPATH, '/html/body/div[1]/main/div/div/div/div/div[2]/div[2]/div[2]/button')
+    download.click()
+
+    # -------------- salvar -----------
+    salvar = 'C:\\Users\\enzzo.nogueira\\Desktop\\bot_selenium\\salvar.png'
+    p.locateCenterOnScreen(salvar)
+    while salvar != True:
+        p.locateCenterOnScreen(salvar)
+        time.sleep(1)
+    p.click(salvar)
+    # python_logo-removebg-preview.png => nome do arquivo salvo
